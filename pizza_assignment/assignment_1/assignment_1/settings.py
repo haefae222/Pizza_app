43570838b8b6a64b75568d5a14b54d423dd729be
Faye Harlick <faye.harlick2@mail.dcu.ie>
2025-02-04 19:02:55 +0000

    adding geodjango and editing settings

--- pizza_assignment/assignment_1/assignment_1/settings.py
+++ pizza_assignment/assignment_1/assignment_1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'pizza_app',
 ]
 
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'assignment_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
--- pizza_assignment/assignment_1/pizza_app/models.py
+++ pizza_assignment/assignment_1/pizza_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.gis.db import models
 #... any other imports
 
 # this is for our actual classes, such as users and pizzas
@@ -102,3 +103,26 @@ class payment(models.Model):
     card_number = models.CharField(max_length=12)
     card_date = models.CharField(max_length=4)
     cvv = models.CharField(max_length=3)
+
+
+class WorldBorder(models.Model):
+    # Regular Django fields corresponding to the attributes in the
+    # world borders shapefile.
+    name = models.CharField(max_length=50)
+    area = models.IntegerField()
+    pop2005 = models.IntegerField("Population 2005")
+    fips = models.CharField("FIPS Code", max_length=2, null=True)
+    iso2 = models.CharField("2 Digit ISO", max_length=2)
+    iso3 = models.CharField("3 Digit ISO", max_length=3)
+    un = models.IntegerField("United Nations Code")
+    region = models.IntegerField("Region Code")
+    subregion = models.IntegerField("Sub-Region Code")
+    lon = models.FloatField()
+    lat = models.FloatField()
+
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    mpoly = models.MultiPolygonField()
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.name
