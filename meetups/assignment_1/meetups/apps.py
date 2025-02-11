e9faea3f6fc8df1248816feb26d4aa0ac1d6a7d8
Faye Harlick <faye.harlick2@mail.dcu.ie>
2025-02-11 10:45:34 +0000

    renaming the project

---
+++ meetups/assignment_1/meetups/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+# this is just the main app class i think
+
+class PizzaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meetups'
