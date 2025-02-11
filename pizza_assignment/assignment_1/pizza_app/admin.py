f1c28eee4822d6ca17de94e96743c8b6c067d168
Faye Harlick <faye.harlick2@mail.dcu.ie>
2025-02-11 10:45:59 +0000

    renaming

--- pizza_assignment/assignment_1/pizza_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import *
-
-# these are for classes? or something
-
-
-# Register your models here.
-admin.site.register(User, UserAdmin)
--- pizza_assignment/assignment_1/pizza_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-# this is just the main app class i think
-
-class PizzaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pizza_app'
